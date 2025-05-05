c776b9557250247c65f413a12fa869f4a2f46ce0
Arc3nal <sebastianblanco760@gmail.com>
2025-05-04 20:47:43 -0500

    avance2

---
+++ Proyecto-electricidad/src/main/java/com/mycompany/proyecto/model/Cliente.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    private final String id; // Número único de identificación
+    private String nombre;
+    private String direccion;
+    private List<Registrador> registradores;
+
+    public Cliente(String id, String nombre, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.registradores = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<Registrador> getRegistradores() {
+        return registradores;
+    }
+
+    public void agregarRegistrador(Registrador r) {
+        this.registradores.add(r);
+    }
+
+    public Registrador buscarRegistrador(String idReg) {
+        for (Registrador r : registradores) {
+            if (r.getId().equals(idReg)) return r;
+        }
+        return null;
+    }
+}
---
+++ Proyecto-electricidad/src/main/java/com/mycompany/proyecto/view/Main.java
@@ -0,0 +1,10 @@
+package view;
+
+import controller.ConsumoControlador;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsumoControlador controlador = new ConsumoControlador();
+        controlador.iniciar();
+    }
+}
